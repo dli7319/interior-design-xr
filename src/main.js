02e060cd43bc924898f3e0babeb97404038578e3
David Li <davili@google.com>
2025-11-14 23:10:31 -0800

    Add meshy test model

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import * as xb from "xrblocks";
 import { Painter } from "./Painter.js";
 import { SpawnInEffect } from "./SpawnInEffect.js";
 
+const CORSPROXY_PREFIX = "https://corsproxy.io/?url=";
+const MESHY_TEST_MODEL =
+  CORSPROXY_PREFIX +
+  "https://assets.meshy.ai/b374fcb7-0ea2-4bb2-a1f3-8f7c26a2c47e/tasks/019a864d-b626-7e0c-9ef4-baea287d8a11/output/model.glb?Expires=1763449268&Signature=DF2Cz4IwfyWxRCKNruRPXTJfmYoikdztEg3MNiC0~gWtUzKoMuJmnd1TJOs3O5r3qxZ1WqhoZYi14XDN8sBHZVynxn-P-N-G6u1eDmKYMFchO-NGPjAkvf6SXYbnrqdXcEqnjBfbBfpWzE4dK9i2X6ZyLZxk-5mjCiXTW5vvb6WtcojZNrLd4~pi0ZP2ODzwrJnpg-06VLKUVfsJSTxgaQJWJ0rIlyUeJtTIe~7G0Ce1N13Dh1rtEOg2w2f90vxpqGXCsjuFcxToMKIybgJ7HXrMEZB43yBBhZgPAk2rI9oexx7qqhtTQ~gqseAZvubZJsQCQsfo7MpdSaEcVf0WTw__&Key-Pair-Id=KL5I0C8H7HX83";
+
 class InteriorDesignApp extends xb.Script {
   init() {
     this.add(new THREE.HemisphereLight(0xffffff, 0x666666, /*intensity=*/ 3));
@@ -21,7 +26,7 @@ class InteriorDesignApp extends xb.Script {
       },
       data: {
         scale: { x: 1, y: 1, z: 1 },
-        model: "Regal_Warrior_1115053429_texture.glb",
+        model: MESHY_TEST_MODEL,
       },
       renderer: xb.core.renderer,
     });
