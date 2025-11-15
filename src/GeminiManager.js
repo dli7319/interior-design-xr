ffe6117ffb1ff28cfec48ec7b128b4129b470984
David Li <davili@google.com>
2025-11-15 14:27:29 -0800

    Increase gemini live schedule head time.

--- src/GeminiManager.js
+++ src/GeminiManager.js
@@ -2,6 +2,34 @@ import { GeminiManager as XBGeminiManager } from "xrblocks/addons/ai/GeminiManag
 import * as xb from "xrblocks";
 
 export class GeminiManager extends XBGeminiManager {
+  // Increase schedule ahead time.
+  scheduleAudioBuffers() {
+    const SCHEDULE_AHEAD_TIME = 1.0;
+    while (
+      this.audioQueue.length > 0 &&
+      this.nextAudioStartTime <=
+        this.audioContext.currentTime + SCHEDULE_AHEAD_TIME
+    ) {
+      const audioBuffer = this.audioQueue.shift();
+      const source = this.audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(this.audioContext.destination);
+      source.onended = () => {
+        source.disconnect();
+        this.queuedSourceNodes.delete(source);
+        this.scheduleAudioBuffers();
+      };
+
+      const startTime = Math.max(
+        this.nextAudioStartTime,
+        this.audioContext.currentTime
+      );
+      source.start(startTime);
+      this.queuedSourceNodes.add(source);
+      this.nextAudioStartTime = startTime + audioBuffer.duration;
+    }
+  }
+
   startScreenshotCapture() {
     this.screenshotInterval2 = setInterval(async () => {
       const base64Image = await xb.core.screenshotSynthesizer.getScreenshot(
